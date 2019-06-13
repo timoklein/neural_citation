d9bb7f09874f27877719b2b354f35282157e21d6
Timo Klein <tkleia34@gmail.com>
2019-06-13 08:27:00 +0200

    added TDNN encoder class and changed documentation format

--- NCN/ncn.py
+++ NCN/ncn.py
@@ -21,14 +21,6 @@ class TDNN(nn.Module):
     - *filter_size* (int): filter length for the convolutional operation  
     - *embed_size* (int): Dimension of the input word embeddings  
     - *num_filters* (int=64): Number of convolutional filters  
-
-    **Input**:  
-
-    - *Tensor* of shape: [N: batch size, D: embedding dimensions, L: sequence length].  
-
-    **Output**:  
-
-    - *Tensor* of shape: [batch_size, num_filters]. 
     """
 
     def __init__(self, filter_size: int, embed_size: int, num_filters: int = 64):
@@ -39,7 +31,13 @@ class TDNN(nn.Module):
 
     def forward(self, x):
         """
-        Forward pass.
+        **Input**:  
+
+        - *Tensor* of shape: [N: batch size, D: embedding dimensions, L: sequence length].  
+
+        **Output**:  
+
+        - *Tensor* of shape: [batch_size, num_filters]. 
         """
         # output shape: [N: batch size, 1: channels, D: embedding dimensions, L: sequence length]
         x = x.unsqueeze(1)
@@ -56,15 +54,64 @@ class TDNN(nn.Module):
         return torch.einsum("nchw -> nhcw", x)
 
 
-# Pad all sequences to equal length so the attention mechanism work -> We don't need this
+class TDNNEncoder(nn.Module):
+    """
+    Encoder Module based on the TDNN architecture.  
+    
+    **Parameters**:  
+    
+    - *filters* (Filters): List of integers determining the filter lengths.    
+    - *num_filters* (int): Number of filters applied in the TDNN convolutional layers.  
+    - *embed_size* (int): Dimensions of the used embeddings.  
+    - *bach_size* (int): Training batch size. 
+    """
+    def __init__(self, filters: Filters,
+                       num_filters: int,
+                       embed_size: int,
+                       batch_size: int):
+
+        super().__init__()
+        self.filter_list = filters
+        self.num_filters = num_filters
+        self.bs = batch_size
+        self._num_filters_total = len(filters)*num_filters
+
+        self.encoder = [TDNN(filter_size=f, embed_size = embed_size, num_filters=num_filters) 
+                                for f in self.filter_list]
+        self.fc = nn.Linear(self._num_filters_total, self._num_filters_total)
+
+    def forward(self, x):
+        """
+        **Input**:  
+
+        - *Tensor* of shape: [N: batch size, D: embedding dimensions, L: sequence length].  
+
+        **Output**:  
+
+        - *Tensor* of shape: [batch_size, number of filter sizes, num_filters].
+        """
+        # output: List of tensors w. shape: batch size, 1, num_filters, 1
+        x = [encoder(x) for encoder in self.encoder]
+
+        # output shape: batch_size, list_length, num_filters
+        x = torch.cat(x, dim=1).squeeze()
+
+        # output shape: batch_size, list_length*num_filters
+        x = x.view(self.bs, -1)
+
+        # apply nonlinear mapping
+        x = torch.tanh(self.fc(x))
+
+        # output shape: batch_size, list_length, num_filters
+        return x.view(-1, len(self.filter_list), self.num_filters)
+
+
+
 # Why do we nee an encoder and decoder Embedding?
 # Because even though we use English as language for input and output,
 # the words used are in the contexts and the cited paper's titles.
 # This is especially pronounced when using a small vocabulary (like 20k words).
-# TODO: Define min and max length based on data -> We need only the max and we don't need to pad :)
 # TODO: Check how we can get only the last relevant output
-# TODO: Implement bucketing to avoid excess computation due to 0 padding -> We don't need that
-# TODO: Masking the loss function??? Why??
 
 
 class AttnDecoderRNN(nn.Module):
@@ -76,32 +123,39 @@ class AttnDecoderRNN(nn.Module):
     
     **Parameters**:  
     
-    - *param1* (type):  
-    
-    **Input**:  
-    
-    - Input 1: [shapes]  
-    
-    **Output**:  
-    
-    - Output 1: [shapes]  
+    - *hidden_size* (int): Dimensions of GRU's hidden state.  
+    - *output_size* (int): Output dimensions of the last linear layer (vocab_size)  
+    - *dropout p* (float=0.2): Probability for dropout regularization. If 0, no regularization is applied.
+        Dropout is also applied to the recurrent layers.  
+    - *max_length* (int): Maximum sequence length of the input (# of attention weights).   
     """
-    def __init__(self, hidden_size: int, output_size: int, dropout_p=0.2, max_length: int = MAX_LENGTH):
+    def __init__(self, hidden_size: int, 
+                       output_size: int, 
+                       dropout_p: int = 0.2, 
+                       layers: int = 1,
+                       max_length: int = MAX_LENGTH):
         super().__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.dropout_p = dropout_p
-        self.max_length = max_length
         self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn = nn.Linear(self.hidden_size * 2, max_length)
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
-        self.dropout = nn.Dropout(self.dropout_p)
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.dropout = nn.Dropout(dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size, dropout= dropout_p, num_layers=layers)
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, input, hidden, encoder_outputs):
+        """
+        **Inputs**:  
+    
+        - Input 1: [shapes]  
+        
+        **Outputs**:  
+        
+        - Output 1: [shapes] 
+        """
         embedded = self.embedding(input).view(1, 1, -1)
         embedded = self.dropout(embedded)
 
@@ -140,20 +194,12 @@ class NCN(nn.Module):
     - *num_layers* (int=1): Number of RNN layers.  
     - *hidden_dims* (int=64): Dimension of the RNN hidden states.  
     - *batch_size* (int=32): Training batch size.  
-    
-    **Input**:  
-    
-    - *Tensor* of shape: [N: batch size, D: embedding dimensions, L: sequence length].   
-    
-    **Output**:  
-    
-    - Output 1: [shapes] 
     """
     def __init__(self, context_filters: Filters,
                        author_filters = Filters,
                        num_filters: int = 64,
                        authors: bool = False, 
-                       w_embed_size: int = 300,
+                       embed_size: int = 300,
                        num_layers: int = 1,
                        hidden_dims: int = 64,
                        batch_size: int = 32):
@@ -164,8 +210,6 @@ class NCN(nn.Module):
         self.author_filter_list = author_filters
         self.num_filters = num_filters # num filters for context == num filters for authors
         self.bs = 32
-        self._num_context_filters_total = len(context_filters)*num_filters
-        self._num_author_filters_total = len(author_filters)*num_filters
 
         # ncn logging stuff
         self.logger = logging.getLogger(__name__)
@@ -174,62 +218,36 @@ class NCN(nn.Module):
         # TODO: Train own context embeddings from dictionary
 
         # context encoder
-        self.context_encoder = [TDNN(filter_size=f, embed_size = w_embed_size, num_filters=num_filters) 
-                                for f in self.context_filter_list]
-        self.fc_context = nn.Linear(self._num_context_filters_total, self._num_context_filters_total)
+        self.context_encoder = TDNNEncoder(self.context_filter_list, self.num_filters, embed_size, self.bs)
 
         if self.use_authors:
             # TODO: Train own author embeddings from dictionary
 
-            # citing author encoder
-            self.authors_citing = [TDNN(filter_size=f, embed_size = w_embed_size, num_filters=num_filters) 
-                                   for f in self.author_filter_list]
-            self.fc_authors_citing = nn.Linear(self._num_author_filters_total, self._num_author_filters_total)
-
-            # cited author encoder
-            self.authors_cited = [TDNN(filter_size=f, embed_size = w_embed_size, num_filters=num_filters) 
-                                  for f in self.author_filter_list]
-            self.fc_authors_cited = nn.Linear(self._num_author_filters_total, self._num_author_filters_total)
+            self.citing_author_encoder = TDNNEncoder(self.author_filter_list, self.num_filters, embed_size, self.bs)
+            self.cited_author_encoder = TDNNEncoder(self.author_filter_list, self.num_filters, embed_size, self.bs)
 
         # TODO: Instantiate AttentionDecoder (Does this have to be different depending on authors?)
 
     def forward(self, context, title, authors_citing=None, authors_cited=None):
+        """
+        **Inputs**:  
+    
+        - *Tensor* of shape: [N: batch size, D: embedding dimensions, L: sequence length].   
+        
+        **Output**:  
+        
+        - Output 1: [shapes] 
+        """
 
-        self.logger.info("Using Author information")
-
-        # context encoder
-        # output: List of tensors w. shape: batch size, 1, num_filters, 1
-        context = [encoder(context) for encoder in self.context_encoder]
-        # output shape: batch_size, list_length, num_filters
-        context = torch.cat(context, dim=1).squeeze()
-        # output shape: batch_size, list_length*num_filters
-        context = context.view(self.bs, -1)
-
-        # apply nonlinear mapping
-        context = torch.tanh(self.fc_context(context))
-        context = context.view(-1, len(self.context_filter_list), self.num_filters)
+        context = self.context_encoder(context)
 
         if self.use_authors and authors_citing is not None and authors_cited is not None:
+            self.logger.info("Using Author information")
 
-            # encode citing authors
-            authors_citing = [encoder(authors_citing) for encoder in self.authors_citing]
-            authors_citing = torch.cat(authors_citing, dim=1).squeeze()
-            authors_citing = authors_citing.view(self._bs, -1)
-            authors_citing = torch.tanh(self.fc_authors_citing(authors_citing))
-            authors_citing = authors_citing.view(-1, len(self.author_filter_list), self.num_filters)
-
-            # encode citing authors
-            authors_cited = [encoder(authors_cited) for encoder in self.authors_cited]
-            authors_cited = torch.cat(authors_cited, dim=1).squeeze()
-            authors_cited = authors_cited.view(self._bs, -1)
-            authors_cited = torch.tanh(self.fc_authors_cited(authors_cited))
-            authors_cited = authors_cited.view(-1, len(self.author_filter_list), self.num_filters)
-
+            authors_citing = self.citing_author_encoder(authors_citing)
+            authors_cited = self.cited_author_encoder(authors_cited)
             # TODO: Concatenate all inputs correctly
             
 
 
-        #------------------------------------------------------------------
-        # decode
-
-        return context # What does this thing actually return????
+        return context # What does this thing actually return???? -> NLLLoss???
