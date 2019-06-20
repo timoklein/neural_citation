9e082d3ae3123defbbb1030ae176a94290723367
Timo Klein <tkleia34@gmail.com>
2019-06-20 11:56:50 +0200

    implemented basic preprocessing for non-author data

--- NCN/utils.py
+++ NCN/utils.py
@@ -1,11 +1,16 @@
 import re
 import pandas as pd
-import spacy
 import logging
 import json
 from pandas import DataFrame
 from pathlib import Path
 from typing import Union, Collection, List, Dict
+from collections import Counter
+import spacy
+from spacy.lemmatizer import Lemmatizer
+from spacy.lang.en import LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES
+from spacy.tokenizer import Tokenizer
+
 
 
 logging.basicConfig(level=logging.INFO, style='$')
@@ -197,7 +202,57 @@ def prepare_data(path: PathOrStr) -> None:
     dataset.drop("index", axis=1, inplace=True)
     dataset.to_pickle(save_dir/f"arxiv_data.pkl", compression=None)
 
+def preprocess_dataset(path: PathOrStr) -> None:
+    """
+    Insert your description here.  
+    
+    ## Parameters:  
+    
+    - **param1** *(type)*:  
+    
+    ## Input:  
+    
+    - **Input 1** *(shapes)*:  
+    
+    ## Output:  
+    
+    - **Output 1** *(shapes)*:  
+    """
+    path = Path(path)
+    data = pd.read_pickle(path)
+
+    # prune empty fields
+    data = data[(data["title_citing"] != "") & 
+                (data["context"] != "") & 
+                (data["authors_citing"] != "") & 
+                (data["title_cited"] != "") & 
+                (data["authors_cited"] != "")]
+    
+    # instantiate spacy model and preprocessers
+    nlp = spacy.load("en_core_web_lg")
+    tokenizer = Tokenizer(nlp.vocab)
+    lemmatizer = Lemmatizer(LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES)
+
+    # preprocessing steps for contexts and cited titles
+    for col in ["context", "title_cited"]:
+        # lower case and strip
+        data.loc[:, col] = data.loc[:, col].str.lower()
+        data.loc[:, col] = data.loc[:, col].str.strip()
+        # lemmatize, stopword removal, punctuation removal
+        data.loc[:, col] = data.loc[:, col].apply(lambda x: [token.lemma_ for token in tokenizer(x) if not token.is_stop and not token.is_punct])
+        # removing numbers and blanks
+        data.loc[:, col] = data.loc[:, col].apply(lambda x: list(filter(lambda tok: tok.strip(), x)))
+        data.loc[:, col] = data.loc[:, col].apply(lambda x: list(filter(lambda tok: tok.isalpha(), x)))
+
+    # TODO: Print results upon complention (Vocab sizes)
+    # TODO: Preprocessing for authors (do all steps except lemmatization -> check number of authors)
+
+    data.to_pickle(path/f"processed_data.pkl", compression=None)
+
+
 
 if __name__ == '__main__':
-    path_to_data = "/home/timo/DataSets/KD_arxiv_CS/arxiv-cs"
-    prepare_data(path_to_data)
+    # path_to_data = "/home/timo/DataSets/KD_arxiv_CS/arxiv-cs"
+    # prepare_data(path_to_data)
+    path_to_df = "/home/timo/DataSets/KD_arxiv_CS/arxiv_data.pkl"
+    preprocess_dataset(path_to_df)
--- README.md
+++ README.md
@@ -12,3 +12,5 @@ https://github.com/tebesu/NeuralCitationNetwork.
 * Remove 1 data sample throwing regex error.   
 * Remove 161670 context samples where information was missing/could not be parsed from files.  
 * This leaves 502353 context - citation pairs with full information.  
+* Vocabulary contexts after processing: 60552.  
+* Vocabulary titles after processing: 31605.  
