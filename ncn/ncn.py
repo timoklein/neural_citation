0b15df4ad862c54e093614c48866c0ae6c423351
Timo Klein <tkleia34@gmail.com>
2019-07-06 09:42:41 +0200

    added working attention layer

--- ncn/ncn.py
+++ ncn/ncn.py
@@ -115,7 +115,6 @@ class TDNNEncoder(nn.Module):
         return x.view(-1, len(self.filter_list), self.num_filters)
 
 
-# TODO: Fix this to work with model
 class Attention(nn.Module):
     def __init__(self, enc_hid_dim, dec_hid_dim):
         super().__init__()
@@ -123,47 +122,43 @@ class Attention(nn.Module):
         self.enc_hid_dim = enc_hid_dim
         self.dec_hid_dim = dec_hid_dim
         
-        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
+        self.attn = nn.Linear(enc_hid_dim + dec_hid_dim, dec_hid_dim)
         self.v = nn.Parameter(torch.rand(dec_hid_dim))
         
-    def forward(self, hidden, encoder_outputs, mask):
+    def forward(self, hidden, encoder_outputs):
         
-        #hidden = [batch size, dec hid dim]
-        #encoder_outputs = [src sent len, batch size, enc hid dim * 2]
-        #mask = [batch size, src sent len]
+        # we start with hidden initialized to zero as our encoder has no hidden dim
+        # hidden = [batch size, dec hid dim]
+        # encoder_outputs = [batch size, num_filters, # filters]
         
-        batch_size = encoder_outputs.shape[1]
-        src_len = encoder_outputs.shape[0]
+        batch_size = encoder_outputs.shape[0]
+        src_len = encoder_outputs.shape[1]
         
-        #repeat encoder hidden state src_len times
+        # repeat encoder hidden state src_len times
         hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
         
-        encoder_outputs = encoder_outputs.permute(1, 0, 2)
-        
-        #hidden = [batch size, src sent len, dec hid dim]
-        #encoder_outputs = [batch size, src sent len, enc hid dim * 2]
+        # hidden = [batch size, src sent len, dec hid dim]
+        # size is already fitting in our model
+        # encoder_outputs = [batch size, src sent len, # filters]
         
         energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim = 2))) 
         
-        #energy = [batch size, src sent len, dec hid dim]
-                
+        # energy = [batch size, src sent len, dec hid dim]
         energy = energy.permute(0, 2, 1)
         
-        #energy = [batch size, dec hid dim, src sent len]
+        # energy = [batch size, dec hid dim, src sent len]
         
-        #v = [dec hid dim]
+        # v = [dec hid dim]
         
         v = self.v.repeat(batch_size, 1).unsqueeze(1)
         
-        #v = [batch size, 1, dec hid dim]
-            
+        # v = [batch size, 1, dec hid dim]
+                
         attention = torch.bmm(v, energy).squeeze(1)
         
-        #attention = [batch size, src sent len]
+        # attention= [batch size, src len]
         
-        attention = attention.masked_fill(mask == 0, -1e10)
-        
-        return F.softmax(attention, dim = 1)
+        return torch.softmax(attention, dim=1)
 
 
 # TODO: Fix this to work with model
